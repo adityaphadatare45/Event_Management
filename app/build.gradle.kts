13c015419705215f8201cc46f369efc85c4f640b
adityaphadatare45 <adityaphadatare4545@gmail.com>
2024-04-20 15:50:56 +0530

    Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,12 +33,18 @@ android {
 }
 
 dependencies {
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
 
+    // Add the dependency for the Realtime Database library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-database")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/main/java/com/example/eventmanagement/Utility.java
@@ -0,0 +1,31 @@
+package com.example.eventmanagement;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+
+public class Utility {
+
+    static void showToast(Context context,String message){
+        Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+    }
+ // Gets the user data from firebase and construct the reference to fire store collection
+    static CollectionReference getCollectionReferenceForNotes(){
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        return FirebaseFirestore.getInstance().collection("notes")
+                .document(currentUser.getUid()).collection("my_notes");
+    }
+
+    static String timestampToString(Timestamp timestamp){
+        // timestamp method helps to convert timestamps stored in Firestore to a human-readable date format.
+        return new SimpleDateFormat("MM/dd/yyyy").format(timestamp.toDate());
+    }
+
+}
